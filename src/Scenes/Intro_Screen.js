d6e3c1eacdb17ec6f2e42ff1c1782e989855e773
Wadswow <nicholas102spy@gmail.com>
2025-05-12 23:08:29 -0700

    intro done

--- src/Scenes/Intro_Screen.js
+++ src/Scenes/Intro_Screen.js
@@ -9,9 +9,10 @@ class Intro extends Phaser.Scene {
         this.load.image("background", "wmremove-transformed.png");
         this.load.image("chick", "Chicken-5-12-2025.png");
         this.load.image("defence", "Defence-5-12-2025.png");
+        this.load.image("start", "game_buttons-removebg-preview.png");
+        this.load.atlasXML("animals", "kenney_animal-pack-redux/Spritesheet/round_nodetails.png", "kenney_animal-pack-redux/Spritesheet/round_nodetails.xml");
     }
     create(){
-        let my = this.my;
         this.add.image(0, 0, "background").setOrigin(0, 0);
         const chick = this.add.image(380, 120, "chick");
         this.tweens.add({
@@ -29,9 +30,30 @@ class Intro extends Phaser.Scene {
             yoyo: true,
             repeat: -1,
         });
+        const start = this.add.image(390, 450, "start").setInteractive();
+        start.setScale(0.3);
+        start.on('pointerover', () => {
+            start.setScale(.33);
+        });
+        start.on('pointerout', () => {
+            start.setScale(.3);
+        });
+        start.on('pointerdown', () => {
+            start.setScale(0.27);
+            start.setAlpha(0.8);
+        });
+        start.on('pointerup', () => {
+            start.setScale(0.3);
+            start.setAlpha(1);
+            this.scene.start('shooterScene');
+        });
+        const avatar1 = this.add.image(180, 460, "animals", "chicken.png");
+        avatar1.angle = -25;
+        const avatar2 = this.add.image(600, 460, "animals", "chicken.png");
+        avatar2.angle = 25;
 
     }
     update(){
-
+        
     }
 }
