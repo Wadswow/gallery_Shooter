ab2676347bc3dd449c8fedd1cb3bb5e52cf0ff20
Wadswow <nicholas102spy@gmail.com>
2025-05-15 01:02:33 -0700

    finished

--- src/Scenes/Game_shooter.js
+++ src/Scenes/Game_shooter.js
@@ -51,12 +51,19 @@ class Game extends Phaser.Scene {
         this.Dkey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.Spacekey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         //text
+        this.score = 0;
         this.scoreText = this.add.text(360, 20, 'Score: ' + this.score, {
             fontFamily: 'Arial',
             fontSize: '24px',
             color: '#000000',
             fontStyle: 'bold'
-        });        
+        });
+        this.hpText = this.add.text(20, 20, 'HP: ' + this.char.hp, {
+            fontFamily: 'Arial',
+            fontSize: '24px',
+            color: '#000000',
+            fontStyle: 'bold'
+        });
         //shooting mechanics
         this.Spacekey.on('down', () => {
             let bullet = this.add.image(this.char.x, this.char.y-20, 'p_bullet').setScale(0.3).setFlipY(true);
@@ -88,7 +95,7 @@ class Game extends Phaser.Scene {
         this.enemyCreation('snake', pathzig, pathzig.startPoint.x, 50, 12000, 5000);
         this.enemyCreation('moose', pathzig, pathzig.startPoint.x, 50, 7000, 20000);
         this.enemyCreation('sloth', randstr, randstr.startPoint.x, 50, 12000, 2000);
-        this.enemyCreation('rhino', randstr, randstr.startPoint.x, 50, 7000, 15000);
+        this.enemyCreation('rhino', randstr, randstr.startPoint.x, 50, 6000, 15000);
         this.enemyCreation('gorilla', randstr, randstr.startPoint.x, 50, 15000, 4000);
         this.enemyCreation('chick', randstr, randstr.startPoint.x, 50, 12000, 10000);
     }
@@ -203,18 +210,31 @@ class Game extends Phaser.Scene {
                     enemy.hp -= 1;
                     if (enemy.hp <= 0){
                         this.score += this.enemyScore[enemy.type];
+                        this.scoreText.setText('Score: ' + this.score);
                         this.despawnE(enemy);
                         this.respawnE(enemy);
                     }
                     break;
                 }
-                if (enemy.y > 598){
-                    this.despawnE(enemy);
-                    this.respawnE(enemy);
-                    this.char.hp -= 1;
-                    if (this.char.hp <= 0){
-                        this.dead();
+            }
+        }
+        for (let i = this.pathEnemy.length - 1; i >= 0; i--){
+            let enemy = this.pathEnemy[i];
+            if (!enemy.active || !enemy.visible) continue;
+            if (enemy.y > 598){
+                this.despawnE(enemy);
+                this.respawnE(enemy);
+                if (enemy.type === 'chick'){
+                    this.char.hp += 1;
+                    if (this.char.hp == 5){
+                        this.char.hp = 5;
                     }
+                } else {
+                    this.char.hp -= 1;
+                }
+                this.hpText.setText('HP: ' + this.char.hp);
+                if (this.char.hp <= 0){
+                    this.dead();
                 }
             }
         }
@@ -225,6 +245,7 @@ class Game extends Phaser.Scene {
                 rock.destroy();
                 this.rocks.splice(i, 1);
                 this.char.hp -= 1;
+                this.hpText.setText('HP: ' + this.char.hp);
                 if (this.char.hp <= 0){
                     this.dead();
                 }
--- src/Scenes/Restart.js
+++ src/Scenes/Restart.js
@@ -7,20 +7,30 @@ class Restart extends Phaser.Scene {
         this.load.image("restart", "game_buttons_restart_-removebg-preview.png");
     }
     create(){
+        this.scoreText = this.add.text(300, 100, 'Score: ' + this.registry.get('score'), {
+            fontFamily: 'Arial',
+            fontSize: '44px',
+            color: '#FFFFFF',
+            fontStyle: 'bold'
+        });
+        this.add.text(50, 250, 'Well, you did what you could to defend the coop,\n you think you can do better?\n Then try again', {
+            align: 'center',
+            fontSize: '24px'
+        });
         const restart = this.add.image(390, 450, "restart").setInteractive();
-        restart.setScale(0.3);
+        restart.setScale(0.5);
         restart.on('pointerover', () => {
-            restart.setScale(.33);
+            restart.setScale(.53);
         });
         restart.on('pointerout', () => {
-            restart.setScale(.3);
+            restart.setScale(.5);
         });
         restart.on('pointerdown', () => {
-            restart.setScale(0.27);
+            restart.setScale(0.47);
             restart.setAlpha(0.8);
         });
         restart.on('pointerup', () => {
-            restart.setScale(0.3);
+            restart.setScale(0.5);
             restart.setAlpha(1);
             this.scene.start('gameScene');
         });
